a89caae302c6a216ebd364994d5e6f6e8fa73d55
Sascha Schneider <sir.foomy@googlemail.com>
2017-12-25 19:07:56 +0100

    Adds testing sources to sources.list

--- Vagrantfile
+++ Vagrantfile
@@ -1,4 +1,5 @@
 Vagrant.configure("2") do |config|
   config.vm.box = "debian/stretch64"
   config.vm.box_version = "9.3.0"
+  config.vm.provision :shell, path: "bootstrap.sh"
 end
---
+++ bootstrap.sh
@@ -0,0 +1,10 @@
+
+# setting up testing in order to get PHP 7.1
+
+sudo su
+
+echo >> /etc/apt/sources.list
+echo "deb http://ftp.uni-kl.de/debian/ testing main" >> /etc/apt/sources.list
+echo "deb-src http://ftp.uni-kl.de/debian/ testing main" >> /etc/apt/sources.list
+
+exit
