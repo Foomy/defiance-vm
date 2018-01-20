70661762c097983bc1c3f15c7b50c8e21ee3646a
Sascha Schneider <sir.foomy@googlemail.com>
2018-01-20 11:11:59 +0100

    Adds mountpoint for a first repo

--- Vagrantfile
+++ Vagrantfile
@@ -1,5 +1,9 @@
 Vagrant.configure("2") do |config|
   config.vm.box = "debian/stretch64"
   config.vm.box_version = "9.3.0"
+  
   config.vm.provision :shell, path: "bootstrap.sh"
+
+  config.vm.synced_folder "/Users/SirFoomy/repos/indexed-db", "/var/www/indexed-db"
+  config.vm.synced_folder "/Users/SirFoomy/repos/sirfoomy", "/var/www/sirfoomy"
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -12,6 +12,11 @@ echo "deb-src http://ftp.uni-kl.de/debian/ testing main" >> /etc/apt/sources.lis
 apt-get update
 apt-get install -y apache2
 apt-get install -y mariadb-server mariadb-client mariadb-common
-apt-get install -y ibapache2-mod-php7.1 php7.1-cli php7.1-cgi php7.1-common php7.1-mysql php7.1-xml php7.1-curl php7.1-gd php7.1-intl php7.1-mcrypt php7.1-zip 
+apt-get install -y libapache2-mod-php7.1 php7.1-cli php7.1-cgi php7.1-common php7.1-mysql php7.1-xml php7.1-curl php7.1-gd php7.1-intl php7.1-mcrypt php7.1-zip 
+
+cp /vagrant/config.dist/apache/vhost/sirfoomy.conf /etc/apache2/sites-available/
+ln -s /etc/apache2/sites-available/sirfoomy.conf /etc/apache2/sites-enabled/sirfoomy.conf
+
+apache2ctl restart
 
 exit
